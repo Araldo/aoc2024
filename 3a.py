509603fbeefbfae0b114abcd9d9b17dcb6e6143c
Araldo van de Kraats <a.vandekraats@gmail.com>
2024-12-03 15:40:45 +0000

    solved day 3

---
+++ 3a.py
@@ -0,0 +1,16 @@
+from utils.download import get_input
+from utils.parse import parse_input_simple
+import re
+
+DAY = 3
+
+get_input(DAY)
+input = parse_input_simple(DAY)
+input = ''.join(input)
+
+matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", input)
+sum = 0
+for match in matches:
+    sum += int(match[0])*int(match[1])
+
+print(sum)
--- utils/parse.py
+++ utils/parse.py
@@ -6,5 +6,8 @@ def parse_input_simple(day: int) -> list[Any]:
     with open(f"{day}.txt", "r") as file:
         data = str(file.readlines()[0])[2:]
         data = data.split("\\n")[:-1]
-        data = [[int(str(item)) for item in line.split()] for line in data]
+        try:
+            data = [[int(str(item)) for item in line.split()] for line in data]
+        except ValueError:
+            return data
     return data
